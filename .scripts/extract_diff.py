f730eea28edadc9a6b03de81b06187ab145e1802
robojumper <robojumper@gmail.com>
2020-10-23 19:41:43 +0200

    Rework

--- .scripts/extract_diff.py
+++ .scripts/extract_diff.py
@@ -12,19 +12,17 @@ def handle_art_url(artifact):
         zip = zipfile.ZipFile(BytesIO(r.content))
         patch = zip.open(sys.argv[1])
         print(
-            '**Workflow failed because Compiletest file needs changes**\n\n',
-            '<details><summary>Patch (click to expand)</summary>\n\n```patch',
+            '**Pull request modifies event listener templates**\n\n',
+            '<details><summary>Difference (click to expand)</summary>\n\n```diff',
             sep='')
         print(patch.read().decode('utf-8'))
         print(
             '```\n</details>\n\n<details><summary>What? (click to expand)</summary>\n\n',
-            'The repository contains some test files to verify that event listeners are documented correctly.',
-            'If you changed any event documentation, the event listener dump needs to be updated.',
-            'The documentation tool has determined that the above changes are needed to update the event listener dump; ',
-            'alternatively you can download the patch file from the ',
-            '[failed run page](' + sys.argv[3] + ') as an artifact ',
-            'or run the `makeDocs` task locally to update the file. This doesn\'t need to happen immediately, ',
-            'just at some point before merging this PR.</details>',
+            'The Highlander documentation tool generates event listener examples from event specifications. ',
+            'This comment contains the modifications that would be made to event ',
+            'listeners for PR authors and reviewers to inspect for correctness and will ',
+            'automatically be kept up-to-date whenever this PR is updated.</details>\n\n',
+            '<!-- GHA-event-listeners-diff -->',
             sep='')
         # early exit
         sys.exit(0)
